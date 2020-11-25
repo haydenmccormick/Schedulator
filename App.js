7ecc3c142ae31350b6352d9cf9404af62eb75521
mk <>
2020-11-24 21:43:22 -0800

    Alert

--- App.js
+++ App.js
@@ -1,16 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Calendar from 'react-calendar'
+import 'react-calendar/dist/Calendar.css';
 
+const disabledDates = ["tomorrow"];
+
+function tileDisabled({ date, view }) {
+  // Disable tiles in month view only
+  if (view === 'month') {
+    // Check if a date React-Calendar wants to check is on the list of disabled dates
+    return disabledDates.find(dDate => isSameDay(dDate, date));
+  }
+}
 export default function App() {
-  return (
-	  <View style={styles.container}>
-	  <Calendar/>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    const [value, setValue] = useState(new Date());
+    function onChange(nextValue) {
+	setValue(nextValue);
+	alert(nextValue);
+    }
+    return (
+	    <View style={styles.container}>
+	    <Calendar
+	onChange={onChange}
+	value={value}
+	/>
+	  <Text>Open up App.js to start working on your app!</Text>
+	  <StatusBar style="auto" />
 	  </View>
-  );
+    );
 }
 
 const styles = StyleSheet.create({
