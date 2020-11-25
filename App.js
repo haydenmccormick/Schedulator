92de90f122f2902eb1358f2eb0d50db4bb1394c9
mk <>
2020-11-24 22:41:49 -0800

    Basic form

--- App.js
+++ App.js
@@ -17,17 +17,45 @@ export default function App() {
     const [value, setValue] = useState(new Date());
     function onChange(nextValue) {
 	setValue(nextValue);
-	alert(nextValue);
+	//alert(nextValue);
+	//const enteredName = prompt('Please enter your name');
     }
     return (
 	    <View style={styles.container}>
 	    <h1>Basic Calendar</h1>
 	    <Calendar onChange={onChange} value={value} />
 	    <h2>Events</h2>
+	    <> <NameForm /> </>
 	    <Text>Open up App.js to start working on your app!</Text>
 	    <StatusBar style="auto" />
 	    </View>
     );
+}
+
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {    this.setState({value: event.target.value});  }
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+	<label>
+	  Name:
+	  <input type="text" value={this.state.value} onChange={this.handleChange} />        </label>
+	<input type="submit" value="Submit" />
+      </form>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
