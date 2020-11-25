3b82f84e7437a164288808971fa4ee0923548aae
mk <>
2020-11-24 21:47:21 -0800

    Events

--- App.js
+++ App.js
@@ -21,13 +21,12 @@ export default function App() {
     }
     return (
 	    <View style={styles.container}>
-	    <Calendar
-	onChange={onChange}
-	value={value}
-	/>
-	  <Text>Open up App.js to start working on your app!</Text>
-	  <StatusBar style="auto" />
-	  </View>
+	    <h1>Basic Calendar</h1>
+	    <Calendar onChange={onChange} value={value} />
+	    <h2>Events</h2>
+	    <Text>Open up App.js to start working on your app!</Text>
+	    <StatusBar style="auto" />
+	    </View>
     );
 }
 
