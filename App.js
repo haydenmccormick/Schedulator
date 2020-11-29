1fd93fdb072d9b051e3261cc7bc7e70869829b49
mk <>
2020-11-29 12:59:23 -0800

    Adding checkbox

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { Component, useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Switch, Button, SafeAreaView, ScrollView } from 'react-native';
+import { CheckBox, StyleSheet, Text, View, TextInput, Switch, Button, SafeAreaView, ScrollView } from 'react-native';
 import { Calendar } from 'react-native-calendars'
 import { DateInput } from 'react-native-date-input';
 import DatePicker from 'react-native-datepicker';
@@ -10,6 +10,7 @@ import TimePicker from 'react-native-simple-time-picker';
 import TimeInterval from 'react-native-clock-interval';
 import * as SQLite from 'expo-sqlite';
 import * as FileSystem from 'expo-file-system';
+
 //import {openDatabase} from 'react-native-sqlite-storage';
 
 FileSystem.downloadAsync(
@@ -49,6 +50,8 @@ export default function App() {
 	    <View style={styles.inputarea}>
 	    <Text style={styles.subtitle}>Add a task</Text>
 	    <Form />
+	    <Text style={styles.subtitle}>Add a dynamic task</Text>
+	    <Dynamic />
 	    </View>
 	    <StatusBar style="auto" />
 	    </View>
@@ -141,6 +144,86 @@ function Form(props) {
 	    </View>
 	    {button}
 	    </View>
+    );
+}
+
+function Dynamic(props) {
+    //Remember to clear form
+    //insert into dynamicTasks(taskname,date,endTime,period,split) values ('a','b','c','d','e');
+    const [input,setInput] = useState("");
+    const [count,setCount] = useState("");
+    const [Hours,setHours] = useState("");
+    const [Minutes,setMinutes] = useState("");
+    const [Hours2,setHours2] = useState("");
+    const [Minutes2,setMinutes2] = useState("");
+    const [toggleCheckBox, setToggleCheckBox] = useState(true);
+    function fixMinutes(minutes) {
+	if (String(minutes).length == 1) {
+	    return '0' + minutes;
+	}
+	else return minutes;
+    }
+    function check() {
+	if (input == "" || count == "" || Hours == "" || Minutes == "" || Hours2 == "" || Minutes2 == "") {
+	    return 0;
+	}
+	return 1;
+    }
+    function notFilled() {
+	alert("Fill in all the fields to add the task");
+    }
+    function submit() {
+	let values = "('" + input + "'" + ",'" + count + "','" + Hours + ":" + Minutes + "','" + Hours2 + ':' + Minutes2 + "')";
+	//Add stuff to database
+	//insert into tasks(taskname,date,startTime,endTime) values ('e','f','g','h'')
+	/*db.transaction(tx => {
+	    tx.executeSql("insert into tasks(taskname,date,startTime,endTime) values" + values,[]);
+	    tx.executeSql(
+			  "select * from tasks",
+			  [],
+			  (_, { rows: { _array } }) => alert(JSON.stringify(_array))
+			  );
+	});*/
+	alert(values);
+    }
+    let button;
+    if (check()) {
+	button = <Button onPress={() => {submit();
+					 setInput("");setCount("");
+					 setHours("");setMinutes("");
+					 setHours2("");setMinutes2("");
+					}} title="Submit" />;
+    }
+    else {
+	button = <Button onPress={() => {notFilled()}} title="Submit" />;
+    }
+    return(
+	    <View>
+	    <Text>Enter task name</Text>
+	    <View>
+	    <TextInput style={styles.input} name="taskname" type="text" value={input} onChangeText={(text) => setInput(text)} />
+	    </View>
+	    <Text>Enter date</Text>
+	    <View>
+	    <DatePicker placeholder={count} onDateChange={(date) => setCount(date)} />
+	    </View>
+	    <Text>Enter deadline time</Text>
+	    <Text>{Hours}:{Minutes}</Text>
+	    <View>
+	    <TimePicker selectedHours={Hours} selectedMinutes={Minutes}
+	onChange={(hours, minutes) => {setHours(hours);setMinutes(fixMinutes(minutes))}} />
+	    </View>
+	    <Text>Enter the amount of time it taskes to get done</Text>
+	    <Text>{Hours2}:{Minutes2}</Text>
+	    <View>
+	    <TimePicker selectedHours={Hours2} selectedMinutes={Minutes2}
+	onChange={(hours, minutes) => {setHours2(hours);setMinutes2(fixMinutes(minutes))}} />
+	    </View>
+	    <Text>Splitable</Text>
+	    <CheckBox disabled={false} value={toggleCheckBox}
+	onValueChange={(newValue) => setToggleCheckBox(newValue)} />
+	    {button}
+	</View>
     );
 }
 
