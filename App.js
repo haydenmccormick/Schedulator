d4547e3d8f932f47d926d3a83fd6d33896185487
Hussein Hassan <h.hassan@ucla.edu>
2020-11-29 21:28:31 +0200

    Save changes

--- App.js
+++ App.js
@@ -1,156 +1,159 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Calendar from 'react-calendar'
-import 'react-calendar/dist/Calendar.css';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 
 const disabledDates = ["tomorrow"];
 
 function tileDisabled({ date, view }) {
   // Disable tiles in month view only
-  if (view === 'month') {
+  if (view === "month") {
     // Check if a date React-Calendar wants to check is on the list of disabled dates
-    return disabledDates.find(dDate => isSameDay(dDate, date));
+    return disabledDates.find((dDate) => isSameDay(dDate, date));
   }
 }
 export default function App() {
-    const [value, setValue] = useState(new Date());
-    function onChange(nextValue) {
-	setValue(nextValue);
-	alert(nextValue);
-	//const enteredName = prompt('Please enter your name');
-    }
-    return (
-	    <View style={styles.container}>
-	    <h1>Basic Calendar</h1>
-	    <Calendar onChange={onChange} value={value} />
-	    <h2>Add a Task</h2>
-	    <> <StaticTasks /> </>
-	    <h2>Events</h2>
-	    <Text>Open up App.js to start working on your app!</Text>
-	    <StatusBar style="auto" />
-	    </View>
-    );
+  const [value, setValue] = useState(new Date());
+  function onChange(nextValue) {
+    setValue(nextValue);
+    alert(nextValue);
+    //const enteredName = prompt('Please enter your name');
+  }
+  return (
+    <View style={styles.container}>
+      <h1>Basic Calendar</h1>
+      <Calendar onChange={onChange} value={value} />
+      <h2>Add a Task</h2>
+      <>
+        {" "}
+        <StaticTasks />{" "}
+      </>
+      <h2>Events</h2>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
 class StaticTasks extends React.Component {
-    constructor(props) {
-	super(props);
-	this.state = {
-	    taskname: null,
-	    date: null,
-	    startTime: null,
-	    endTime: null,
-	};
+  constructor(props) {
+    super(props);
+    this.state = {
+      taskname: null,
+      date: null,
+      startTime: null,
+      endTime: null,
+    };
 
-	this.handleInputChange = this.handleInputChange.bind(this);
-    }
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
 
-    handleInputChange(event) {
-	const target = event.target;
-	const name = target.name;
-	const value = target.value;
-	switch (name) {
-	case "taskname":
-	    this.taskname = value;
-	    break;
-	case "date":
-	    this.date = value;
-	    break;
-	case "startTime":
-	    this.startTime = value;
-	    break;
-	case "endTime":
-	    this.endTime = value;
-	    break;
-	case "submit":
-	    if (this.taskname != undefined && this.taskname != "" && this.taskname != null &&
-		this.date != undefined && this.date != "" && this.date != null &&
-		this.startTime != undefined && this.startTime != "" && this.startTime != null &&
-		this.endTime != undefined && this.endTime != "" && this.endTime != null
-	       ) {
-		alert("Task has been created");
-		this.taskname = null;
-		this.date = null;
-		this.startTime = null;
-		this.endTime = null;
-		//Add stuff to database here
-		target.type = "reset";
-	    }
-	    else {
-		alert("You must fill all the forms before creating a task");
-		target.type = "button";
-	    }
-	}
+  handleInputChange(event) {
+    const target = event.target;
+    const name = target.name;
+    const value = target.value;
+    switch (name) {
+      case "taskname":
+        this.taskname = value;
+        break;
+      case "date":
+        this.date = value;
+        break;
+      case "startTime":
+        this.startTime = value;
+        break;
+      case "endTime":
+        this.endTime = value;
+        break;
+      case "submit":
+        if (
+          this.taskname != undefined &&
+          this.taskname != "" &&
+          this.taskname != null &&
+          this.date != undefined &&
+          this.date != "" &&
+          this.date != null &&
+          this.startTime != undefined &&
+          this.startTime != "" &&
+          this.startTime != null &&
+          this.endTime != undefined &&
+          this.endTime != "" &&
+          this.endTime != null
+        ) {
+          alert("Task has been created");
+          this.taskname = null;
+          this.date = null;
+          this.startTime = null;
+          this.endTime = null;
+          //Add stuff to database here
+          target.type = "reset";
+        } else {
+          alert("You must fill all the forms before creating a task");
+          target.type = "button";
+        }
     }
+  }
 
-    render() {
-	return (
-		<form onsubmit="return false">
-		<label>
-		Task Name:
-		<input
-	    name="taskname"
-	    type="text"
-	    value={this.tasknameValue}
-	    onChange={this.handleInputChange}
-		/>
-		</label>
-		<br />
-		Date:
-		<label>
-		<input
-	    name="date"
-	    type="date"
-	    onChange={this.handleInputChange}
-		/>
-		</label>
-		<br />
-		<label>
-		Start Time:
-		<input
-	    name="startTime"
-	    type="time"
-	    onChange={this.handleInputChange}
-	    />
-		</label>
-		<br />
-		<label>
-		End Time:
-		<input
-	    name="endTime"
-	    type="time"
-	    onChange={this.handleInputChange}
-	    />
-		</label>
-		<br />
-		<label>
-		<input
-	    name="reset"
-	    type="reset"
-	    value="Reset form"
-	    onClick={this.handleInputChange}
-		/>
-		</label>
-		<br />
-		<label>
-		<input
-	    name="submit"
-	    type="button"
-	    value="Add static task"
-	    onClick={this.handleInputChange}
-	    />
-		</label>
-		</form>
-	);
-    }
+  render() {
+    return (
+      <form onsubmit="return false">
+        <label>
+          Task Name:
+          <input
+            name="taskname"
+            type="text"
+            value={this.tasknameValue}
+            onChange={this.handleInputChange}
+          />
+        </label>
+        <br />
+        Date:
+        <label>
+          <input name="date" type="date" onChange={this.handleInputChange} />
+        </label>
+        <br />
+        <label>
+          Start Time:
+          <input
+            name="startTime"
+            type="time"
+            onChange={this.handleInputChange}
+          />
+        </label>
+        <br />
+        <label>
+          End Time:
+          <input name="endTime" type="time" onChange={this.handleInputChange} />
+        </label>
+        <br />
+        <label>
+          <input
+            name="reset"
+            type="reset"
+            value="Reset form"
+            onClick={this.handleInputChange}
+          />
+        </label>
+        <br />
+        <label>
+          <input
+            name="submit"
+            type="button"
+            value="Add static task"
+            onClick={this.handleInputChange}
+          />
+        </label>
+      </form>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- Scheduler.js
+++ Scheduler.js
@@ -9,7 +9,34 @@ While meetiong the user's configuration as well as possible (daily work hours,
 Note that events pushed into this function should have a 10 minute beffer attached
 */
 
-function schedule(calendar, events) {
+function schedule(calendar, events, usrPref) {
   // note that the calendar shows the blocks taken up by static events
-  var sortEvents = events.sort();
+  //events contains a random array of all events
+  events = events.sort((a, b) => {
+    dateToNum(a.getDate()) - dateToNum(b.getDate);
+  }); //sorts events in ascending order of date
+  //note for later, events here sorted so that in the future the cost of the sort is lowered
+  //now ordered events are availabke and you start reading them in
+  //split the evnts up into chunks as needed
+  //You want to return a schdule starting te time when this functioj got invoke. Give
+  //the user 15 minutres from now tio start work and set up everything
+
+  //in your config set the buffer period. Default buffer period for events is 15 minutes
+  //usroref.maxlength is the maximum amount of time worked and is usually initialized to 90
+  //usr.avgLength is the average tiome of work before a break and is initialized to 50 minutes
+  for (var i; i < events.length(); i++) {
+    if (events[i].splitable() && events[i].getLength() > usrPref.maxlength) {
+      var initialLength = events[i].getLength();
+      var nPieces = 
+    }
+  }
+}
+
+function dateToNum(date) {
+  // date here is a struct. We convert this to a number
+  var time = date.minute;
+  time += date.hour * 60;
+  time += date.day * 24 * 60;
+  time += date.year * 24 * 60 * 365;
+  return time; //ret7urns time in minutes
 }
