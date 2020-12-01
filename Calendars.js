755349c26695edcd1663373f37ba30ae0d2cc708
mk <>
2020-12-01 12:11:13 -0800

    Showing events

--- Calendars.js
+++ Calendars.js
@@ -1,22 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { View, TouchableOpacity, Image } from 'react-native';
+import { View, TouchableOpacity, Image, Text, Button } from 'react-native';
 import { Calendar, Agenda } from 'react-native-calendars'
-
+import {TableView} from "react-native-responsive-table"
 import { Form, Dynamic } from './EventForm.js'
 import styles from './assets/Styles.js'
+import * as SQLite from 'expo-sqlite';
 
+const db = SQLite.openDatabase("db.db");
 
 function MonthView() {
   const d = new Date();
   const month = d.getMonth() + 1;
   const string = d.getFullYear() + '-' + month + '-' + d.getDate();
-  const [selected, setSelected] = useState(string);
+    const [selected, setSelected] = useState(string);
+     const [tasks, setTasks] = useState("");
+    const [tasks2, setTasks2] = useState("");
   const onDayPress = (day) => {
     setSelected(day.dateString);
     alert(day.dateString);
   };
-
+function showTasks() {
+	db.transaction(tx => {
+	    //tx.executeSql("insert into tasks(taskname,date,startTime,endTime) values" + values, []);
+	    tx.executeSql(
+		"select * from tasks",
+		[],
+		(_, { rows: { _array } }) => setTasks(_array)
+	    );
+	});
+	setTasks2(JSON.stringify(tasks));
+    }
   /***** Executed on "Add" button press, renders an EventForm *****/
   const [addingEvent, setAddingEvent] = useState(false);
   const addEventPressHandler = () => {
@@ -28,33 +42,58 @@ function MonthView() {
       {/* So the user can click outside of form box to cancel*/}
       <TouchableOpacity style={styles.formwrapper} onPress={addEventPressHandler} />
       <View style={styles.formcontainer}>
-        <Form />
+	<Form />
       </View>
     </View>
     : null)
-
+    let tableview;
+    if(tasks != "") {
+	tableview =	  <TableView horizontalScroll={true} columnWidth={70}
+      headers={[
+	{
+	    name:"Taskname",
+	    reference_key:"taskname",
+	},
+	{
+	    name:"Date",
+	    reference_key:"date",
+	},
+	{
+	    name:"Start Time",
+	    reference_key:"startTime",
+	},
+	  {
+	      name:"End Time",
+	      reference_key:"endTime",
+	  },
+      ]}
+      rows={tasks}
+	    />;
+    }
   return (
     <View style={styles.container}>
       <View style={styles.calendararea}>
-        <Calendar
-          onDayPress={onDayPress}
-          markedDates={{
-            [selected]: {
-              selected: true,
-              disableTouchEvent: true,
-              selectedColor: 'lightblue',
-              selectedTextColor: 'black',
-            },
-          }}
-        />
+	<Calendar
+	  onDayPress={onDayPress}
+	  markedDates={{
+	    [selected]: {
+	      selected: true,
+	      disableTouchEvent: true,
+	      selectedColor: 'lightblue',
+	      selectedTextColor: 'black',
+	    },
+	  }}
+	  />
+	  <Button title="Click to show tasks" onPress={() => showTasks()} />
+	  {tableview}
       </View>
       <View style={styles.buttonwrapper}>
-        <TouchableOpacity onPress={addEventPressHandler}>
-          <Image
-            source={require('./assets/event-button.png')}
-            style={styles.button}
-          />
-        </TouchableOpacity>
+	<TouchableOpacity onPress={addEventPressHandler}>
+	  <Image
+	    source={require('./assets/event-button.png')}
+	    style={styles.button}
+	  />
+	  </TouchableOpacity>
       </View>
       <StatusBar style="auto" />
       {render_form}
@@ -64,7 +103,7 @@ function MonthView() {
 
 function DayView() {
     return (
-        <Agenda />
+	<Agenda />
     );
 }
 
