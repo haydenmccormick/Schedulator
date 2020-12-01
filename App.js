24b55353389cb9dc169004f1d234abf1660aa222
mk <>
2020-12-01 11:07:48 -0800

    Moved stuff around

--- App.js
+++ App.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
+import * as FileSystem from 'expo-file-system';
 import { MonthView, DayView } from './Calendars.js'
+import * as SQLite from 'expo-sqlite';
 
 const Tab = createBottomTabNavigator();
+
+FileSystem.downloadAsync(
+    Expo.Asset.fromModule(require('./db.db')).uri,
+    `${FileSystem.documentDirectory}SQLite/db.db`
+  );
+const db = SQLite.openDatabase("db.db");
 
 export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Day" component={DayView} />
-        <Tab.Screen name="Month" component={MonthView} style={{backgroundColor: 'white',}}/>
+	<Tab.Screen name="Day" component={DayView} />
+	<Tab.Screen name="Month" component={MonthView} style={{backgroundColor: 'white',}}/>
       </Tab.Navigator>
     </NavigationContainer>
   );
