a62c3d1ebe11356c9b16f506c0e3b471b852e3ad
mk <>
2020-12-01 17:34:07 -0800

    added stuff to tasks.js

--- Tasks.js
+++ Tasks.js
@@ -1,13 +1,57 @@
-import React from 'react';
 import { View, Text } from 'react-native';
 import styles from './assets/Styles';
+import * as SQLite from 'expo-sqlite';
+import { TableView } from "react-native-responsive-table"
+import React, { useState } from 'react';
+
+const db = SQLite.openDatabase("db.db");
+
 
 export default function TaskList() {
+    const [tasks, setTasks] = useState("");
+	function showDates() {
+	const tempDates={};
+	db.transaction(tx => {
+		tx.executeSql(
+		"select * from tasks",
+		[],
+		(_, { rows: { _array } }) => setTasks(_array)
+	    );
+	});
+	}
+	let tableview;
+    if (tasks != "") {
+	//horizontalScroll={true} columnWidth={50} height={150}
+	tableview = <TableView
+	    headers={[
+		{
+		    name: "Taskname",
+		    reference_key: "taskname",
+		},
+		{
+		    name: "Date",
+		    reference_key: "date",
+		},
+		{
+		    name: "Start Time",
+		    reference_key: "startTime",
+		},
+		{
+		    name: "End Time",
+		    reference_key: "endTime",
+		},
+	    ]}
+	    rows={tasks}
+	/>;
+    }
+    else {
+	tableview = <Text>When you add a task, it will be displayed here</Text>
+    }
     return (
-        <View>
-            <Text>
-                Tasks
-            </Text>
-        </View>
+	    <View style={{padding: 30}}>
+	    <Text>
+	    {tableview}
+	    </Text>
+	    </View>
     );
 }
