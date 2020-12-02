1ceef376fc3f6348817548e84e5a874b1b3b6bbd
Hayden McCormick <haydenmccormick@haydens-mbp.lan>
2020-12-01 20:54:28 -0600

    Add Empty Data render

--- Calendars.js
+++ Calendars.js
@@ -6,110 +6,94 @@ import { TableView } from "react-native-responsive-table"
 import { Form, Dynamic } from './EventForm.js'
 import styles from './assets/Styles.js'
 import * as SQLite from 'expo-sqlite';
+import { useFonts, Roboto_300Light, Roboto_400Regular } from '@expo-google-fonts/roboto';
 
 const db = SQLite.openDatabase("db.db");
 
 function DayView() {
-    const d = new Date();
-    const month = d.getMonth() + 1;
-    const string = d.getFullYear() + '-' + month + '-' + d.getDate();
-    const [selected, setSelected] = useState(string);
-    const [tasks, setTasks] = useState("");
-    const [tasks2, setTasks2] = useState("");
-    const [dates, setDates] = useState([{}]);
-    const onDayPress = (day) => {
-	setSelected(day.dateString);
-	alert(day.dateString);
-    };
+	useFonts({Roboto_300Light, Roboto_400Regular});
+	const d = new Date();
+	const month = d.getMonth() + 1;
+	const string = d.getFullYear() + '-' + month + '-' + d.getDate();
+	const [selected, setSelected] = useState(string);
+	const [tasks, setTasks] = useState("");
+	const [dates, setDates] = useState([{}]);
+	const onDayPress = (day) => {
+		setSelected(day.dateString);
+		alert(day.dateString);
+	};
 
-    function showDates() {
-	const tempDates={};
-	db.transaction(tx => {
-	    //tx.executeSql("insert into tasks(taskname,date,startTime,endTime) values" + values, []);
-	    tx.executeSql(
-		"select * from tasks",
-		[],
-		(tx, results) => {
-		    for (let i = 0; i < results.rows.length; i++)
-		    {
-			tempDates[results.rows.item(i).date] = {marked: true};
-		    }
-		    setDates(tempDates);
-		}
-	    );
-		tx.executeSql(
-		"select * from tasks",
-		[],
-		(_, { rows: { _array } }) => setTasks(_array)
-	    );
-	});
-    }
+	function showDates() {
+		const tempDates = {};
+		db.transaction(tx => {
+			//tx.executeSql("insert into tasks(taskname,date,startTime,endTime) values" + values, []);
+			tx.executeSql(
+				"select * from tasks",
+				[],
+				(tx, results) => {
+					for (let i = 0; i < results.rows.length; i++) {
+						tempDates[results.rows.item(i).date] = { marked: true };
+					}
+					setDates(tempDates);
+				}
+			);
+			tx.executeSql(
+				"select * from tasks",
+				[],
+				(_, { rows: { _array } }) => setTasks(_array)
+			);
+		});
+	}
 
-    /***** Executed on "Add" button press, renders an EventForm *****/
-    const [addingEvent, setAddingEvent] = useState(false);
-    const addEventPressHandler = () => {
-	showDates();
-	setAddingEvent(!addingEvent);
-    };
-    // Only render a form if the user is adding an event
-    const render_form = (addingEvent ?
-	<View style={styles.formwrapper}>
-	    {/* So the user can click outside of form box to cancel*/}
-	    <TouchableOpacity style={styles.formwrapper} onPress={addEventPressHandler} />
-	    <View style={styles.formcontainer}>
-		<Form retFunc={addEventPressHandler}/>
-	    </View>
-	</View>
-	: null)
-    let tableview;
-    if (tasks != "") {
-	//horizontalScroll={true} columnWidth={50} height={150}
-	tableview = <TableView
-	    headers={[
-		{
-		    name: "Taskname",
-		    reference_key: "taskname",
-		},
-		{
-		    name: "Date",
-		    reference_key: "date",
-		},
-		{
-		    name: "Start Time",
-		    reference_key: "startTime",
-		},
-		{
-		    name: "End Time",
-		    reference_key: "endTime",
-		},
-	    ]}
-	    rows={tasks}
-	/>;
-    }
-    else {
-	tableview = <Text>When you add a task, it will be displayed here</Text>
-    }
-    return (
-	<View style={styles.container}>
-		<Agenda style={styles.container}
-		    onDayPress={onDayPress}
-			markedDates={dates}
-			items={{'2020-12-02': [{name: 'work ye loada shite'}]}}
-		/>
-	    <View style={styles.buttonwrapper}>
-		<TouchableOpacity onPress={addEventPressHandler}>
-		    <Image
-			source={require('./assets/event-button.png')}
-			style={styles.button}
-		    />
-		</TouchableOpacity>
-	    </View>
-	    <StatusBar style="auto" />
-	    {render_form}
-	</View>
-    );
+	/***** Executed on "Add" button press, renders an EventForm *****/
+	const [addingEvent, setAddingEvent] = useState(false);
+	const addEventPressHandler = () => {
+		showDates();
+		setAddingEvent(!addingEvent);
+	};
+	// Only render a form if the user is adding an event
+	const render_form = (addingEvent ?
+		<View style={styles.formwrapper}>
+			{/* So the user can click outside of form box to cancel*/}
+			<TouchableOpacity style={styles.formwrapper} onPress={addEventPressHandler} />
+			<View style={styles.formcontainer}>
+				<Form retFunc={addEventPressHandler} />
+			</View>
+		</View>
+		: null)
+
+
+	const emptyday = (
+		<View style={styles.eventcontainer}>
+			<Text style={styles.eventtext}>
+				There's nothing here. Add an event to liven things up!
+			</Text>
+		</View>
+	)
+	return (
+		<View style={styles.container}>
+			<Agenda style={styles.container}
+				onDayPress={onDayPress}
+				markedDates={dates}
+				renderEmptyData={() => {return emptyday;}}
+			/>
+			<View style={styles.buttonwrapper}>
+				<TouchableOpacity onPress={addEventPressHandler}>
+					<Image
+						source={require('./assets/event-button.png')}
+						style={styles.button}
+					/>
+				</TouchableOpacity>
+			</View>
+			<StatusBar style="auto" />
+			{render_form}
+		</View>
+	);
 }
 
+
+
+
 module.exports = {
-    DayView: DayView,
+	DayView: DayView,
 }
--- assets/Styles.js
+++ assets/Styles.js
@@ -1,72 +1,80 @@
 import { StyleSheet } from "react-native"
 
 export default StyleSheet.create({
-    container: {
-      position: "absolute",
-      top: 0, bottom: 0, left: 0, right: 0,
-    },
-    calendararea: {
-      flex: 1.5,
-      justifyContent: 'flex-end',
-      width: "85%",
-    },
-    going: {
-      alignItems: 'center',
-      marginVertical: 15,
-    },
-    input: {
-      textAlign: 'center',
-      padding: 5,
-      paddingHorizontal: 10,
-      marginVertical: 10,
-      borderColor: 'skyblue',
-      borderWidth: 2,
-    },
-    title: {
-      alignSelf: 'center',
-      fontSize: 25,
-      paddingBottom: 15,
-    },
-    subtitle: {
-      alignSelf: 'center',
-      fontSize: 20,
-      marginBottom: 15,
-      borderBottomWidth: 1.5,
-    },
-    buttonwrapper: {
-        bottom: '5%',
-        right: '8%',
-        flex: 1,
-        alignContent: 'center',
-        justifyContent: 'flex-end',
-        alignItems: 'flex-end',
-    },
-    button: {
-        width: 100,
-        height: 100,
-        resizeMode: 'contain',
-    },
-    formwrapper: {
-        position: 'absolute',
-        top: 0,
-        bottom: 0,
-        left: 0,
-        right: 0,
-        backgroundColor: 'black',
-        backgroundColor: 'rgba(0,0,0,0.5)'
-    },
-    formcontainer: {
-        width: '75%',
-        height: '90%',
-        bottom: '-5%',
-        alignSelf: 'center',
-    },
-    formshell: {
-    },
-    form: {
-        borderRadius: 30,
-        paddingHorizontal: 40,
-        paddingTop: 10,
-        backgroundColor: 'white',
-    },
-  });
+  container: {
+    position: "absolute",
+    top: 0, bottom: 0, left: 0, right: 0,
+  },
+  calendararea: {
+    flex: 1.5,
+    justifyContent: 'flex-end',
+    width: "85%",
+  },
+  going: {
+    alignItems: 'center',
+    marginVertical: 15,
+  },
+  input: {
+    textAlign: 'center',
+    padding: 5,
+    paddingHorizontal: 10,
+    marginVertical: 10,
+    borderColor: 'skyblue',
+    borderWidth: 2,
+  },
+  title: {
+    alignSelf: 'center',
+    fontSize: 25,
+    paddingBottom: 15,
+  },
+  subtitle: {
+    alignSelf: 'center',
+    fontSize: 20,
+    marginBottom: 15,
+    borderBottomWidth: 1.5,
+  },
+  buttonwrapper: {
+    position: 'absolute',
+    bottom: '5%',
+    right: '8%',
+  },
+  button: {
+    width: 85,
+    height: 85,
+    resizeMode: 'contain',
+  },
+  formwrapper: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: 'black',
+    backgroundColor: 'rgba(0,0,0,0.5)'
+  },
+  formcontainer: {
+    width: '75%',
+    height: '90%',
+    bottom: '-5%',
+    alignSelf: 'center',
+  },
+  formshell: {
+  },
+  form: {
+    borderRadius: 30,
+    paddingHorizontal: 40,
+    paddingTop: 10,
+    backgroundColor: 'white',
+  },
+  eventcontainer: {
+    backgroundColor: 'white',
+    alignSelf: "center",
+    marginVertical: 10,
+    marginHorizontal: 10,
+    paddingVertical: 40,
+    paddingHorizontal: 15,
+  },
+  eventtext: {
+    fontFamily: 'Roboto_100Thin',
+  },
+});
