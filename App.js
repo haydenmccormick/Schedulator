c55d0a35a9890fff5f68f794e0b552186aa584c9
mk <>
2020-12-14 14:54:54 -0800

    Format changes

--- App.js
+++ App.js
@@ -19,8 +19,8 @@ const addr = "http://192.168.86.27:8000/";
 const Tab = createBottomTabNavigator();
 
 FileSystem.downloadAsync(
-	Expo.Asset.fromModule(require('./server/db.db')).uri,
-	`${FileSystem.documentDirectory}SQLite/db.db`
+    Expo.Asset.fromModule(require('./server/db.db')).uri,
+    `${FileSystem.documentDirectory}SQLite/db.db`
 );
 
 
@@ -28,107 +28,107 @@ const db = SQLite.openDatabase("db.db");
 
 //tab bar style options
 function options() {
-	tabBarOptions: { showIcon: true }
-	tabBarIcon: (() => {
-		return (<Image
-			style={{ width: 50, height: 50 }}
-			source={{ url: "https://facebook.github.io/react/img/logo_og.png" }} />);
-	}
-	)
+    tabBarOptions: { showIcon: true }
+    tabBarIcon: (() => {
+	return (<Image
+		style={{ width: 50, height: 50 }}
+		source={{ url: "https://facebook.github.io/react/img/logo_og.png" }} />);
+    }
+		)
 }
 
 export default function App() {
 
-	// In {Date: [{event1}, {event2}]} format for display on Agenda
-	const [taskEntries, setTaskEntries] = useState([{}]);
-	const [dateEntries, setDateEntries] = useState({});
+    // In {Date: [{event1}, {event2}]} format for display on Agenda
+    const [taskEntries, setTaskEntries] = useState([{}]);
+    const [dateEntries, setDateEntries] = useState({});
 
-	// In [{event1}, {event2}] format for scheduler/task list
-	const [dynamicTasks, setDynamicTasks] = useState({});
-	const [taskList, setTaskList] = useState({});
+    // In [{event1}, {event2}] format for scheduler/task list
+    const [dynamicTasks, setDynamicTasks] = useState({});
+    const [taskList, setTaskList] = useState({});
     const [loaded, setLoaded] = useState(false);
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [correct, setCorrect] = useState(0);
 
-	// Load tasks to agenda on app startup
-	if (!loaded) {
-		// initializeTasks();
-		findTasks();
-		setLoaded(true);
-	}
-	// Load in expo google fonts
-	let [fontsLoaded] = useFonts({
-		Roboto_100Thin,
-		Roboto_300Light,
-		Roboto_400Regular,
-		AbrilFatface_400Regular
-	});
-	// If it's not loaded in time, make the user wait
-	if (!fontsLoaded) {
-		return <AppLoading />;
-	}
+    // Load tasks to agenda on app startup
+    if (!loaded) {
+	// initializeTasks();
+	findTasks();
+	setLoaded(true);
+    }
+    // Load in expo google fonts
+    let [fontsLoaded] = useFonts({
+	Roboto_100Thin,
+	Roboto_300Light,
+	Roboto_400Regular,
+	AbrilFatface_400Regular
+    });
+    // If it's not loaded in time, make the user wait
+    if (!fontsLoaded) {
+	return <AppLoading />;
+    }
 
-	function getTaskInfo(tasks) {
-		const tempTasks = {};
-		const tempDates = {};
-		const tempList = [];
-		var parsed = tasks;
-	    for (var i in parsed) {
-		//alert(JSON.stringify(parsed[i]));
-			if (parsed[i].type == 'static')
-				tempList.push(parsed[i]);
-			newTask = {
-				name: parsed[i].taskname,
-				date: parsed[i].dateString,
-				startTime: parsed[i].startTime,
-				endTime: parsed[i].endTime,
-				type: parsed[i].type
-			};
-			newDate = { marked: true };
-			if (tempTasks[parsed[i].dateString]) {
-				tempTasks[parsed[i].dateString].push(newTask);
-			}
-			else {
-				tempTasks[parsed[i].dateString] = [newTask];
-				tempDates[parsed[i].dateString] = newDate;
-			}
-		}
-		setTaskEntries(tempTasks);
-		setDateEntries(tempDates);
-		setTaskList(tempList);
+    function getTaskInfo(tasks) {
+	const tempTasks = {};
+	const tempDates = {};
+	const tempList = [];
+	var parsed = tasks;
+	for (var i in parsed) {
+	    //alert(JSON.stringify(parsed[i]));
+	    if (parsed[i].type == 'static')
+		tempList.push(parsed[i]);
+	    newTask = {
+		name: parsed[i].taskname,
+		date: parsed[i].dateString,
+		startTime: parsed[i].startTime,
+		endTime: parsed[i].endTime,
+		type: parsed[i].type
+	    };
+	    newDate = { marked: true };
+	    if (tempTasks[parsed[i].dateString]) {
+		tempTasks[parsed[i].dateString].push(newTask);
+	    }
+	    else {
+		tempTasks[parsed[i].dateString] = [newTask];
+		tempDates[parsed[i].dateString] = newDate;
+	    }
 	}
+	setTaskEntries(tempTasks);
+	setDateEntries(tempDates);
+	setTaskList(tempList);
+    }
 
-	function getDynamicInfo(tasks) {
-		const tempDynamic = [];
-	    for (var i in tasks) {
-			tempDynamic.push(tasks[i]);
-		}
-		setDynamicTasks(tempDynamic);
+    function getDynamicInfo(tasks) {
+	const tempDynamic = [];
+	for (var i in tasks) {
+	    tempDynamic.push(tasks[i]);
 	}
+	setDynamicTasks(tempDynamic);
+    }
 
-	async function pushServer(command) {
-		var body = new FormData();
-		body.append('text', command);
-		var xhr = new XMLHttpRequest();
-		xhr.open('PUT', addr);
-		xhr.send(body);
-		findTasks();
-	}
+    async function pushServer(command) {
+	var body = new FormData();
+	body.append('text', command);
+	var xhr = new XMLHttpRequest();
+	xhr.open('PUT', addr);
+	xhr.send(body);
+	findTasks();
+    }
 
-	async function findTasks() {
-		getTaskInfo(require('./server/all.json'));
-		getDynamicInfo(require('./server/dynamicTasks.json'));
-		schedule(taskList, dynamicTasks);
+    async function findTasks() {
+	getTaskInfo(require('./server/all.json'));
+	getDynamicInfo(require('./server/dynamicTasks.json'));
+	schedule(taskList, dynamicTasks);
 
-	}
+    }
 
-	function gettaskEntries() {
-		return taskEntries;
-	}
-	function getDynamicTaskEntries() {
-		return dynamicTasks;
-	}
+    function gettaskEntries() {
+	return taskEntries;
+    }
+    function getDynamicTaskEntries() {
+	return dynamicTasks;
+    }
 
     //const cred = ["mk","pass"];
     let content;
@@ -171,35 +171,35 @@ export default function App() {
     }
     else{
 	content = <Tab.Navigator
-				screenOptions={options}
-			>
-				<Tab.Screen name="Agenda" children={() => <DayView findTasks={findTasks} tasks={gettaskEntries()}
-								    pushServer={pushServer} getUsername={getUsername()} />}
-					options={{
-						tabBarIcon: ({ color }) => (
-							<Image
-								style={styles.icon}
-								source={require('./assets/Tab_Icons/agenda-on.png')
-								} />
-						),
-					}}
-				/>
+	screenOptions={options}
+	    >
+	    <Tab.Screen name="Agenda" children={() => <DayView findTasks={findTasks} tasks={gettaskEntries()}
+						pushServer={pushServer} getUsername={getUsername()} />}
+	options={{
+	    tabBarIcon: ({ color }) => (
+		    <Image
+		style={styles.icon}
+		source={require('./assets/Tab_Icons/agenda-on.png')
+		       } />
+	    ),
+	}}
+	    />
 	    <Tab.Screen name="Tasks" children={() => <DynamicTaskList findTasks={findTasks} tasks={getDynamicTaskEntries()} getUsername={getUsername()}
-					pushServer={pushServer} static={taskList} />}
-					options={{
-						tabBarIcon: ({ color }) => (
-							<Image
-								style={styles.icon}
-								source={require('./assets/Tab_Icons/tasks-on.png')
-								} />
-						),
-					}}
-				/>
+					       pushServer={pushServer} static={taskList} />}
+	options={{
+	    tabBarIcon: ({ color }) => (
+		    <Image
+		style={styles.icon}
+		source={require('./assets/Tab_Icons/tasks-on.png')
+		       } />
+	    ),
+	}}
+	    />
 	    </Tab.Navigator>;
     }
     return (
-	<NavigationContainer>
+	    <NavigationContainer>
 	    {content}
 	</NavigationContainer>
-	);
+    );
 }
